05017414d5c30a363cc47a816a465d27bf249e10
Kapil Bhale <kapilbhale2001@gail.com>
2021-10-04 20:22:30 +0530

    React Login Page

---
+++ 02_10_WEB_task_11_kapil_bhale/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Component from './component';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02_10_WEB_task_11_kapil_bhale/src/component.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './style.css';
+
+export default function Component() {
+    return (
+        <>
+            <div className="wrapper">
+            <div className="title">Login Form</div>
+            <form action="#">
+                <div className="field">
+                <input type="text" required />
+                <label>Email Address</label>
+                </div>
+                <div className="field">
+                <input type="password" required />
+                <label>Password</label>
+                </div>
+                <div className="content">
+                <div className="checkbox">
+                    <input type="checkbox" id="remember-me" />
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div className="pass-link"><a href="#">Forgot password?</a></div>
+                </div>
+                <div className="field">
+                <input type="submit" value="Login" />
+                </div>
+                <div className="signup-link">Not a member? <a href="#">Signup now</a></div>
+            </form>
+            </div>
+        </>
+    )
+}
